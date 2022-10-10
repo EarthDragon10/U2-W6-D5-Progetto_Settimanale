9269a96dd50cdfd133f626622b85181e4bbdfb30
EarthDragon10 <lagudadev@gmail.com>
2022-10-10 20:13:31 +0200

    fix some js and sass rules

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ const headerElement = document.getElementById("header");
 const mainElement = document.getElementById("main");
 const footerElement = document.getElementById("footer");
 const btnDarkMode = document.getElementById("change-DarkMode");
+const iconSocials = document.querySelectorAll(".socials > div > a");
 const linksItemHeader = document.querySelectorAll("#links ul > li > a");
 
-console.log(linksItemHeader);
+console.log(iconSocials);
 
 function changeDarkMode() {
 	if (btnDarkMode.classList.value === "light-mode") {
@@ -17,35 +18,51 @@ function changeDarkMode() {
 // Usare il metodo replace invece che .add e .remove
 
 function addDarkMode() {
-	headerElement.classList.remove("light-mode");
-	mainElement.classList.remove("light-mode");
-	footerElement.classList.remove("light-mode");
-	btnDarkMode.classList.remove("light-mode");
+	headerElement.classList.replace("light-mode", "dark-mode");
+	mainElement.classList.replace("light-mode", "dark-mode");
+	footerElement.classList.replace("light-mode", "dark-mode");
+	btnDarkMode.classList.replace("light-mode", "dark-mode");
 
-	headerElement.classList.add("dark-mode");
-	mainElement.classList.add("dark-mode");
-	footerElement.classList.add("dark-mode");
-	btnDarkMode.classList.add("dark-mode");
+	// headerElement.classList.remove("light-mode");
+	// mainElement.classList.remove("light-mode");
+	// footerElement.classList.remove("light-mode");
+	// btnDarkMode.classList.remove("light-mode");
+
+	// headerElement.classList.add("dark-mode");
+	// mainElement.classList.add("dark-mode");
+	// footerElement.classList.add("dark-mode");
+	// btnDarkMode.classList.add("dark-mode");
 
 	for (let i = 0; i < linksItemHeader.length; i++) {
-		linksItemHeader[i].classList.remove("light-mode");
-		linksItemHeader[i].classList.add("dark-mode");
+		linksItemHeader[i].classList.replace("light-mode", "dark-mode");
+		iconSocials[i].classList.replace("light-mode", "dark-mode");
+
+		// linksItemHeader[i].classList.remove("light-mode");
+		// linksItemHeader[i].classList.add("dark-mode");
 	}
 }
 
 function addLightMode() {
-	headerElement.classList.remove("dark-mode");
-	mainElement.classList.remove("dark-mode");
-	footerElement.classList.remove("dark-mode");
-	btnDarkMode.classList.remove("dark-mode");
+	headerElement.classList.replace("dark-mode", "light-mode");
+	mainElement.classList.replace("dark-mode", "light-mode");
+	footerElement.classList.replace("dark-mode", "light-mode");
+	btnDarkMode.classList.replace("dark-mode", "light-mode");
 
-	headerElement.classList.add("light-mode");
-	mainElement.classList.add("light-mode");
-	footerElement.classList.add("light-mode");
-	btnDarkMode.classList.add("light-mode");
+	// headerElement.classList.remove("dark-mode");
+	// mainElement.classList.remove("dark-mode");
+	// footerElement.classList.remove("dark-mode");
+	// btnDarkMode.classList.remove("dark-mode");
+
+	// headerElement.classList.add("light-mode");
+	// mainElement.classList.add("light-mode");
+	// footerElement.classList.add("light-mode");
+	// btnDarkMode.classList.add("light-mode");
 
 	for (let i = 0; i < linksItemHeader.length; i++) {
-		linksItemHeader[i].classList.remove("dark-mode");
-		linksItemHeader[i].classList.add("light-mode");
+		linksItemHeader[i].classList.replace("dark-mode", "light-mode");
+		iconSocials[i].classList.replace("dark-mode", "light-mode");
+
+		// linksItemHeader[i].classList.remove("dark-mode");
+		// linksItemHeader[i].classList.add("light-mode");
 	}
 }
